7e135fc0f1f3c69a17b1332e4368d2c43964124e
elment74 <elment74@gmail.com>
2025-06-08 19:10:20 -0700

    debug mods

--- src/index.ts
+++ src/index.ts
@@ -560,7 +560,7 @@ function readChatbox() {
   const opts = reader.read() || [];
   let chatStr = "";
   let chatArr: string[] = [];
-  console.log(chatArr);
+  console.log(opts);
   if (opts.length !== 0) {
     for (const line in opts) {
       if (!opts[line].text.match(timestampRegex) && line === "0") {
